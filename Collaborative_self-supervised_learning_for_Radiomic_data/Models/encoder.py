4f2725ca4cb8f316170fefafef69637f2cdcaafb
Hailong Li <31737423+jiaolang771@users.noreply.github.com>
2025-09-24 16:47:28 -0400

    Add files via upload

---
+++ Collaborative_self-supervised_learning_for_Radiomic_data/Models/encoder.py
@@ -0,0 +1,98 @@
+# code from "https://github.com/guocheng2018/Transformer-Encoder/tree/master/transformer_encoder"
+
+import torch
+import torch.nn as nn
+
+from .encoder_layer import EncoderLayer
+from .feed_forward import FeedForward
+from .layer_norm import LayerNorm
+from .multi_head_attention import MultiHeadAttention
+from .utils import clones
+from typing import Union
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
+        super(FeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+    
+class LayerNorm(nn.Module):
+    def __init__(self, features: int, eps: float = 1e-6):
+        # features = d_model
+        super(LayerNorm, self).__init__()
+        self.a = nn.Parameter(torch.ones(features))
+        self.b = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a * (x - mean) / (std + self.eps) + self.b
+
+
+class EncoderLayer(nn.Module):
+    """Encoder is made up of self-attn and feed forward"""
+
+    def __init__(self, size: int, self_attn: MultiHeadAttention, feed_forward: FeedForward, dropout: float):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x: torch.FloatTensor, mask: torch.ByteTensor) -> torch.FloatTensor:
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+
+    def __init__(self, size: int, dropout: float):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.FloatTensor, sublayer: Union[MultiHeadAttention, FeedForward]) -> torch.FloatTensor:
+        """Apply residual connection to any sublayer with the same size."""
+        return x + self.dropout(sublayer(self.norm(x)))
+
+class Encoder(nn.Module):
+    """Core encoder is a stack of N layers"""
+
+    def __init__(self, layer: EncoderLayer, N: int):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x: torch.FloatTensor, mask: torch.ByteTensor) -> torch.FloatTensor:
+        """Pass the input (and mask) through each layer in turn."""
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, n_heads: int = 1, n_layers: int = 1,
+                 dropout: float = 0.1):
+        super(TransformerEncoder, self).__init__()
+        self.multi_headed_attention = MultiHeadAttention(n_heads, d_model, dropout)
+        self.feed_forward = FeedForward(d_model, d_ff, dropout)
+        self.encoder_layer = EncoderLayer(d_model, self.multi_headed_attention, self.feed_forward, dropout)
+        self.encoder = Encoder(self.encoder_layer, n_layers)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self, x: torch.FloatTensor, mask: torch.ByteTensor) -> torch.FloatTensor:
+        return self.encoder(x, mask)
